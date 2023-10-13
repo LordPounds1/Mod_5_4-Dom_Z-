600568eafd638f6ffe2e5e889d250dded7612438
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-10-13 13:18:17 +0600

    Add project files.

---
+++ Mod_5_4(Dom_Z)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mod_5_4_Dom_z_
+{
+
+    public class MethodCaller
+    {
+        // Создаем метод Generate Exception и обрабатываем любое вызванное им исключение.
+        public void CallGen()
+        {
+            try
+            {
+                GenerateEx();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение найдено: {ex.Message}");
+            }
+        }
+
+        // Генерируем исключение и выбрасываем его.
+        private void GenerateEx()
+        {
+            throw new Exception("Пиво, создано в GenerateEx");
+        }
+    }
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var methodCaller = new MethodCaller();
+            methodCaller.CallGen();
+        }
+    }
+}
